934b20e3abb705e924bd986180dfb25dd9bca837
jenan <jenanmj15@gmail.com>
2024-06-13 07:16:39 +0300

    booking controller and router and server

--- server.js
+++ server.js
@@ -1,29 +1,28 @@
-const express = require("express")
-const logger = require("morgan")
-const cors = require("cors")
+const express = require('express')
+const logger = require('morgan')
+const cors = require('cors')
 
-const AuthRouter = require("./routes/AuthRouter")
-const HotelsRouter = require("./routes/HotelsRouter")
-const RoomsRouter = require("./routes/RoomsRouter")
-
-
+const AuthRouter = require('./routes/AuthRouter')
+const HotelsRouter = require('./routes/HotelsRouter')
+const RoomsRouter = require('./routes/RoomsRouter')
+const BookingRouter = require('./routes/bookingRouter')
 
 const PORT = process.env.PORT || 3001
 
-const db = require("./db")
+const db = require('./db')
 
 const app = express()
 
 app.use(cors())
-app.use(logger("dev"))
+app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
-app.use("/auth", AuthRouter)
-app.use("/", HotelsRouter)
-app.use("/", RoomsRouter)
-
-app.use("/", (req, res) => {
+app.use('/auth', AuthRouter)
+app.use('/', HotelsRouter)
+app.use('/', RoomsRouter)
+app.use('/', BookingRouter)
+app.use('/', (req, res) => {
   res.send(`Connected!`)
 })
 
