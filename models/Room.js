7a58a573556408a4fd95a465c7a5ccb4ea387042
maria0alawadhi <mariaalawadhi@gmail.com>
2024-06-18 17:21:05 +0300

    modified the schemas

--- models/Room.js
+++ models/Room.js
@@ -5,7 +5,6 @@ const roomSchema = new Schema(
     name: String,
     img: String,
     type: String,
-    rating: { type: [Number], min: 0, max: 5 },
     review: [String],
     available: Boolean,
     facilities: [String],
