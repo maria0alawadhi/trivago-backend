9087905290df8141df0c0def9c21c88203fcdb1a
maria0alawadhi <mariaalawadhi@gmail.com>
2024-06-19 16:14:07 +0300

    add review schema

---
+++ models/Review.js
@@ -0,0 +1,14 @@
+const { Schema } = require('mongoose')
+
+const reviewSchema = new Schema(
+  {
+    review: String,
+    rating:Number,
+    user:[{ type: Schema.Types.ObjectId, ref: 'User' }],
+    room:[{ type: Schema.Types.ObjectId, ref: 'Room' }]
+
+  },
+  { timestamps: true }
+)
+
+module.exports = reviewSchema
--- models/Room.js
+++ models/Room.js
@@ -5,7 +5,6 @@ const roomSchema = new Schema(
     name: String,
     img: String,
     type: String,
-    review: [String],
     available: Boolean,
     facilities: [String],
     price: Number
