92d47fb105079d80b821c1b08baed74c84c78080
maria0alawadhi <mariaalawadhi@gmail.com>
2024-06-19 20:15:22 +0300

    reviews controllers are added

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -1,12 +1,27 @@
 const { Review } = require('../models/Index')
 
+
+
+const createReview = async(req, res) => {
+    try {
+      const review = new Review(req.body);
+      await review.save();
+      res.send(review)
+        } catch (err) {
+          console.error(err)
+        }
+  }
+
+
 const getAllReviews = async (req, res) => {
   try {
-    const reviews = await Review.find()
+    const { roomid } = req.params;
+    const reviews = await Review.find({ room: roomid }).populate('user')
+      
     res.send(reviews)
   } catch (error) {
     console.log(error)
-    res.status(500).send('Internal Server Error')
+    res.status(500).send('Error fetching reviews')
   }
 }
 
@@ -28,9 +43,8 @@ const deleteReview = async (req, res) => {
 
 module.exports = {
   getAllReviews,
-  deleteReview
+  deleteReview,
+  createReview
 }
 
-
-router.post('/reviews/:roomid', reservCtrl.createReview)
 
--- routes/reviews.js
+++ routes/reviews.js
@@ -1,9 +1,8 @@
 const router = require('express').Router()
 const reviewsCtrl = require('../controllers/reviews')
 
-
-router.get('/reviews/:roomid', reviewsCtrl.getAllReviews)
-router.post('/reviews/:roomid', reservCtrl.createReview)
-router.delete('/reviews/:roomid', reviewsCtrl.deleteReview)
+router.get('/reviews/:roomid',reviewsCtrl.getAllReviews)
+router.post('/reviews/:roomid',reviewsCtrl.createReview)
+router.delete('/reviews/:roomid',reviewsCtrl.deleteReview)
 
 module.exports = router
